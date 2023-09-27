860ff1e04e4c34a96d01d61547adbcd04f0c642a
waltermity <walthierdavid42@gmail.com>
2023-09-27 15:40:16 +0200

    halohalo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -83,7 +83,7 @@ public class GT4500 implements SpaceShip {
         firingSuccess = primaryTorpedoStore.fire(1);
         firingSuccess = secondaryTorpedoStore.fire(1);
         wasPrimaryFiredLast = false;
-          
+          //halohalo
               break;
     }
 
